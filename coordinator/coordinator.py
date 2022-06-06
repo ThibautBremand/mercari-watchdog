1025f11df0a95889207a2a9e0392da02d28484d2
Thibaut Bremand <thibaut.bremand@gmail.com>
2022-06-06 20:35:27 +0200

    Use cached ids when parsing items

--- coordinator/coordinator.py
+++ coordinator/coordinator.py
@@ -9,46 +9,48 @@ from scraper import searcher
 
 def start(searches: List[config.Search], delay: int):
     while True:
-        data = read_cache()
+        cached_data = read_cache()
 
         scraped = {}
         for s in searches:
-            kw = s['keywords']
-            items = handle_search(kw)
+            kw: str = s['keywords']
+
+            cached_ids_str = ''
+            if kw in cached_data:
+                cached_ids_str = cached_data[kw]
+
+            cached_ids = cached_ids_str.split(cache.cache.last_ids_separator)
+
+            items = handle_search(kw, cached_ids)
             scraped[kw] = items
             time.sleep(2)
 
-        write_cache(scraped)
+        write_cache(scraped, cached_data)
 
         time.sleep(delay)
 
 
-def handle_search(kw):
+def handle_search(kw: str, cached_ids: List[str]):
     print(f'Handling keywords {kw}...')
+    res = []
+    items = searcher.search(kw, cached_ids)
 
-    items = searcher.search(kw)
-    print(f'Found {len(items)} new items for keywords {kw}')
+    for it in items:
+        if it.id in cached_ids:
+            break
+        res.append(it)
 
-    return items
-
-
-def build_cache(scraped):
-    res = {}
-    for kw in scraped:
-        val = scraped[kw]
-        ids = []
-        i = 0
-        for item in val:
-            ids.append(item.id)
-            i += 1
-            if i >= cache.cache.last_ids_size:
-                break
-
-        last_ids = cache.cache.last_ids_separator.join(ids)
-
-        res[kw] = last_ids
+    print(f'Found {len(res)} new items for keywords {kw}')
 
     return res
+
+
+def write_cache(scraped, cached_data):
+    print('Writing cache...')
+    to_persist = build_cache(scraped, cached_data)
+    json.dump(to_persist, open('scraped.txt', 'w'))
+
+    print('Cache written!')
 
 
 def read_cache():
@@ -61,11 +63,29 @@ def read_cache():
     return res
 
 
-def write_cache(scraped):
-    print('Writing cache...')
-    to_persist = build_cache(scraped)
-    # with open('scraped.txt', 'w') as f:
-    #     print(to_persist, file=f)
-    json.dump(to_persist, open('scraped.txt', 'w'))
+def build_cache(scraped, cached_data):
+    res = {}
+    for kw in scraped:
+        val = scraped[kw]
+        ids = []
+        i = 0
+        for item in val:
+            ids.append(item.id)
+            i += 1
+            if i >= cache.cache.last_ids_size:
+                break
 
-    print('Cache written!')
+        # In case we have found less new items than last_ids_size, we add ids from the cache
+        if len(ids) < cache.cache.last_ids_size:
+            if kw in cached_data:
+                cached = cached_data[kw].split(cache.cache.last_ids_separator)
+                for i in cached:
+                    ids.append(i)
+                    if len(ids) >= cache.cache.last_ids_size:
+                        break
+
+        last_ids = cache.cache.last_ids_separator.join(ids)
+
+        res[kw] = last_ids
+
+    return res

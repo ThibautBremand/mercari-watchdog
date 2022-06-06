504d68810a08ac399fccb2990837420079c99cad
Thibaut Bremand <thibaut.bremand@gmail.com>
2022-06-06 19:53:12 +0200

    Read cache

--- coordinator/coordinator.py
+++ coordinator/coordinator.py
@@ -1,9 +1,35 @@
+import json
 import time
 from typing import List
 
 import cache.cache
 from config import config
 from scraper import searcher
+
+
+def start(searches: List[config.Search], delay: int):
+    while True:
+        data = read_cache()
+
+        scraped = {}
+        for s in searches:
+            kw = s['keywords']
+            items = handle_search(kw)
+            scraped[kw] = items
+            time.sleep(2)
+
+        write_cache(scraped)
+
+        time.sleep(delay)
+
+
+def handle_search(kw):
+    print(f'Handling keywords {kw}...')
+
+    items = searcher.search(kw)
+    print(f'Found {len(items)} new items for keywords {kw}')
+
+    return items
 
 
 def build_cache(scraped):
@@ -25,19 +51,21 @@ def build_cache(scraped):
     return res
 
 
-def start(searches: List[config.Search], delay: int):
-    while True:
-        scraped = {}
-        for s in searches:
-            kw = s['keywords']
-            items = searcher.search(kw)
-            scraped[kw] = items
+def read_cache():
+    try:
+        res = json.load(open('scraped.txt'))
+    except:
+        print('could not read cache')
+        return {}
 
-            time.sleep(2)
+    return res
 
-        to_persist = build_cache(scraped)
 
-        with open('scraped.txt', 'w') as f:
-            print(to_persist, file=f)
+def write_cache(scraped):
+    print('Writing cache...')
+    to_persist = build_cache(scraped)
+    # with open('scraped.txt', 'w') as f:
+    #     print(to_persist, file=f)
+    json.dump(to_persist, open('scraped.txt', 'w'))
 
-        time.sleep(delay)
+    print('Cache written!')
